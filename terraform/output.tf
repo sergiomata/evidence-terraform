cc68d336d847e848d7cc480f95b8f888b44510d2
Sergio mata <sergio.mata@ivoy.mx>
2019-09-13 10:22:05 -0500

    feat: add aurora and ec2 outputs

---
+++ terraform/output.tf
@@ -0,0 +1,51 @@
+output "this_rds_cluster_id" {
+  description = "The ID of the cluster"
+  value       = module.aurora.this_rds_cluster_id
+}
+output "this_rds_cluster_resource_id" {
+  description = "The Resource ID of the cluster"
+  value       = module.aurora.this_rds_cluster_resource_id
+}
+output "this_rds_cluster_endpoint" {
+  description = "The cluster endpoint"
+  value       = module.aurora.this_rds_cluster_endpoint
+}
+output "this_rds_cluster_reader_endpoint" {
+  description = "The cluster reader endpoint"
+  value       = module.aurora.this_rds_cluster_reader_endpoint
+}
+output "this_rds_cluster_database_name" {
+  description = "Name for an automatically created database on cluster creation"
+  value       = module.aurora.this_rds_cluster_database_name
+}
+output "this_rds_cluster_master_password" {
+  description = "The master password"
+  value       = module.aurora.this_rds_cluster_master_password
+}
+output "this_rds_cluster_port" {
+  description = "The port"
+  value       = module.aurora.this_rds_cluster_port
+}
+output "this_rds_cluster_master_username" {
+  description = "The master username"
+  value       = module.aurora.this_rds_cluster_master_username
+}
+// aws_rds_cluster_instance
+output "this_rds_cluster_instance_endpoints" {
+  description = "A list of all cluster instance endpoints"
+  value       = module.aurora.this_rds_cluster_instance_endpoints
+}
+// aws_security_group
+output "this_security_group_id" {
+  description = "The security group ID of the cluster"
+  value       = module.aurora.this_security_group_id
+}
+
+output "ec2_security_group_id" {
+  description = "The security group ID for ec2 evidence"
+  value       = aws_security_group.ec2-sg.id
+}
+
+output "ec2_cluster_public_dns" {
+  value = module.ec2_cluster.public_dns
+}
